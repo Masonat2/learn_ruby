9e2a2225c1e0523be29d893fcb4a5d89c9004b0a
Mason <MZ103312@Gmail.com>
2017-09-24 08:00:35 -0700

    Solved all tests

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,35 @@
 #write your code here
+def add(x,y)
+  return x + y
+end
+
+def subtract(x,y)
+  return x-y
+end
+
+def sum(numbers)
+total=0
+   numbers.each {|i|  total += i}
+return total
+end
+
+def multiply(numbers)
+  total=1
+  numbers.each {|i| total*=i}
+  return total
+end
+
+def power(x,y)
+  return x**y
+
+end
+
+def factorial(x)
+  total=1
+while x > 0
+ total*=x
+  x=x-1
+
+end
+return total
+end
