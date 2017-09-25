6c49bcabfa4a4d9a4d91377e95a7a099bd2f5adf
Mason <MZ103312@Gmail.com>
2017-09-24 23:08:21 -0700

    Solved all tests

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,42 @@
 class Timer
-  #write your code here
+
+attr_accessor :seconds
+
+
+
+    def seconds
+      return @seconds.to_i
+    end
+    def time_string
+      secs=@seconds.to_i
+      hours=0
+      minutes=0
+      while secs>59 do
+        minutes = minutes+1
+        secs = secs - 60
+      end
+      while minutes>59 do
+        hours= hours+1
+        minutes = minutes - 60
+      end
+
+
+    if hours>10 && minutes>10 && secs>10
+
+      return "#{hours}"+":"+"#{minutes}"+":"+"#{secs}"
+
+    elsif hours < 10 && minutes < 10 && secs < 10
+
+      return "0"+"#{hours}"+":"+"0"+"#{minutes}"+":"+"0"+"#{secs}"
+    elsif hours < 10 && minutes < 10 && secs >10
+      return "0"+"#{hours}"+":"+"0"+"#{minutes}"+":"+"#{secs}"
+
+    end
+    end
+
+
+
+
+
+
 end
