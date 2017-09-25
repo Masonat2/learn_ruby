27779959835cbee4e68487f28a698950ff6819a0
Mason <MZ103312@Gmail.com>
2017-09-24 21:51:26 -0700

    Solved all tests

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
+# object is book
 class Book
-# write your code here
+
+  attr_accessor :title
+  def title
+  small_words=["and", "or", "but", "over", "nor", "a", "in","the", "of", "an"]
+  letters= @title.split(" ").each do |word|
+    if (small_words.include?(word))
+      word.downcase!
+    else
+      word.capitalize!
+    end
+  end
+    letters[0].capitalize!
+    letters.join(" ")
+  end
 end
